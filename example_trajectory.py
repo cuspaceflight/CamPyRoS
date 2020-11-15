57b07bce45058c764ac84e68573709f75cd03147
dug20 <daniel.u.gibbons@gmail.com>
2020-11-15 21:36:53 +0000

    Added aerodynamic arrows to 3D plot
    Also added pitching to rail - but not sure if it's implemented right

--- example_trajectory.py
+++ example_trajectory.py
@@ -35,7 +35,7 @@ radius = 98.5e-3
 '''Create the objects needed to initialise the Rocket object'''
 mass_model = main.CylindricalMassModel(dry_mass + np.array(prop_mass_data), motor_time_data, length, radius)
 pulsar = main.Motor(motor_time_data, prop_mass_data, cham_pres_data, throat_data, gamma_data, nozzle_efficiency_data, exit_pres_data, area_ratio_data)
-launch_site = main.LaunchSite(rail_length=5, rail_yaw=0, rail_pitch=0, alt=0, longi=0, lat=0, wind=[0,0,0], atmosphere=StandardAtmosphere)
+launch_site = main.LaunchSite(rail_length=5, rail_yaw=0, rail_pitch=0.4, alt=0, longi=0, lat=0, wind=[0,0,0], atmosphere=StandardAtmosphere)
 
 '''Create the Rocket object'''
 martlet4 = main.Rocket(mass_model, pulsar, aerodynamic_coefficients, launch_site, 0.05, False)
@@ -55,7 +55,7 @@ print(min(simulation_output["h"]),max(simulation_output["h"]))
 
 main.plot_orientation(simulation_output)
 main.plot_inertial_trajectory_3d(simulation_output, show_orientation=True)
-main.plot_launch_trajectory_3d(simulation_output, show_orientation=True)
+main.plot_launch_trajectory_3d(simulation_output, show_orientation=True, show_aero=True)
 
 main.animate_orientation(simulation_output)
 
