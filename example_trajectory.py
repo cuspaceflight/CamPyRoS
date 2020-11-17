3a561725a7e6d2a25c408958436072d4c67a0d1e
dug20 <daniel.u.gibbons@gmail.com>
2020-11-17 00:13:47 +0000

    Semi-implicit euler integration added
    Even though it's less accurate, it's much simpler than RK4

--- example_trajectory.py
+++ example_trajectory.py
@@ -1,4 +1,4 @@
-import main,csv
+import main,plot,csv
 from datetime import datetime
 from main import StandardAtmosphere
 import numpy as np
@@ -35,29 +35,29 @@ radius = 98.5e-3
 '''Create the objects needed to initialise the Rocket object'''
 mass_model = main.CylindricalMassModel(dry_mass + np.array(prop_mass_data), motor_time_data, length, radius)
 pulsar = main.Motor(motor_time_data, prop_mass_data, cham_pres_data, throat_data, gamma_data, nozzle_efficiency_data, exit_pres_data, area_ratio_data)
-launch_site = main.LaunchSite(rail_length=5, rail_yaw=0, rail_pitch=0.4, alt=0, longi=0, lat=0, wind=[0,0,0], atmosphere=StandardAtmosphere)
+launch_site = main.LaunchSite(rail_length=5, rail_yaw=0, rail_pitch=0.4, alt=10, longi=0, lat=0, wind=[0,0,0], atmosphere=StandardAtmosphere)
 
 '''Create the Rocket object'''
 martlet4 = main.Rocket(mass_model, pulsar, aerodynamic_coefficients, launch_site, 0.05, False)
 
 '''Run the simulation'''
-simulation_output = main.run_simulation(martlet4)
+simulation_output = main.run_simulation_euler(martlet4)
 #simulation_output.to_csv("results/results_%s.csv"%datetime.now().strftime("%m_%d_%Y_%H_%M_%S"), index=True, mode="w+")
 print(min(simulation_output["h"]),max(simulation_output["h"]))
 
 '''Plot the results'''
-#main.plot_velocity(simulation_output)
-#main.plot_altitude_time(simulation_output)
-#main.plot_aero_forces(simulation_output)
-#main.plot_orientation(simulation_output)
-#main.plot_rot_acc(simulation_output)
-#main.plot_position(simulation_output)
+#plot.plot_velocity(simulation_output)
+#plot.plot_altitude_time(simulation_output)
+#plot.plot_aero_forces(simulation_output)
+#plot.plot_orientation(simulation_output)
+#plot.plot_rot_acc(simulation_output)
+#plot.plot_position(simulation_output)
 
-main.plot_orientation(simulation_output)
-main.plot_inertial_trajectory_3d(simulation_output, show_orientation=True)
-main.plot_launch_trajectory_3d(simulation_output, show_orientation=True, show_aero=True)
+plot.plot_orientation(simulation_output)
+plot.plot_inertial_trajectory_3d(simulation_output, show_orientation=True)
+plot.plot_launch_trajectory_3d(simulation_output, show_orientation=True, show_aero=False)
 
-main.animate_orientation(simulation_output)
+plot.animate_orientation(simulation_output)
 
 
 
