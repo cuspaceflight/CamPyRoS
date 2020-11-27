ca98ce6d3fd68d13b905ac99f2ba5d169ddf8e6d
Jago Strong-Wright <jagoosw@protonmail.com>
2020-11-27 18:09:41 +0000

    Works

--- example_trajectory.py
+++ example_trajectory.py
@@ -37,7 +37,7 @@ radius = 98.5e-3    # m
 '''Create the objects needed to initialise the Rocket object'''
 mass_model = main.CylindricalMassModel(dry_mass + np.array(prop_mass_data), motor_time_data, length, radius)
 pulsar = main.Motor(motor_time_data, prop_mass_data, cham_pres_data, throat_data, gamma_data, nozzle_efficiency_data, exit_pres_data, area_ratio_data)
-launch_site = main.LaunchSite(rail_length=5, rail_yaw=20, rail_pitch=0, alt=0, longi=0, lat=0, wind=[0,0,0], atmosphere=StandardAtmosphere)
+launch_site = main.LaunchSite(rail_length=5, rail_yaw=45, rail_pitch=20, alt=0, longi=0, lat=0, wind=[0,0,0], atmosphere=StandardAtmosphere)
 
 '''Create the Rocket object'''
 martlet4 = main.Rocket(mass_model, pulsar, aerodynamic_coefficients, launch_site, 0.05, True)
