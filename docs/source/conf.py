37a77d50a3e4e57eb0cfef153a51d4abb7db778e
Jago Strong-Wright <jagoosw@protonmail.com>
2020-12-18 22:10:57 +0000

    Fixed wind and changed to way nicer sphinz theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,8 +42,8 @@ extensions = [
     "rst2pdf.pdfbuilder"
 ]
 
-html_theme = "sphinx_rtd_theme"
-
+#html_theme = "sphinx_rtd_theme"
+html_theme = "pydata_sphinx_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
