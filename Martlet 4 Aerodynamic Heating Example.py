a6b64272fbffb44b4c5d571387b9d62efb006703
dug20 <daniel.u.gibbons@gmail.com>
2020-12-22 18:09:06 +0000

    Added test against NASA data
    The "NQLDW019 Reference 8 Test.py"

---
+++ Martlet 4 Aerodynamic Heating Example.py
@@ -0,0 +1,18 @@
+import trajectory, trajectory.post, trajectory.aero, csv
+import numpy as np
+from martlet4 import martlet4
+
+trajectory_data = trajectory.from_json("output.json")
+
+tangent_ogive = trajectory.post.TangentOgive(xprime = 73.7e-2, yprime = (19.7e-2)/2)
+analysis = trajectory.post.HeatTransfer(tangent_ogive, trajectory_data, martlet4)
+analysis.step(print_style="metric")
+
+#analysis.run(iterations = 300, print_style="minimal")
+#analysis.to_json("martlet4_heating.json")
+
+analysis.from_json("martlet4_heating.json")
+
+analysis.plot_station(station_number = 10, imax = 300)
+analysis.plot_heat_transfer(automatic_rescaling=True)
+analysis.plot_fluid_properties(automatic_rescaling=True)
