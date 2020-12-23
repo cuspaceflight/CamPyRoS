976520df9cfb33dee670c4834237c8b4cb7c118b
dug20 <daniel.u.gibbons@gmail.com>
2020-12-23 13:15:03 +0000

    Fixed q_turb
    There was a typo in the NASA document that I copied across - I fixed it and it seems to work better now.
    
    It seems to genereally overpredict the heat rates relative to NASA documents.

--- NASA TND889.py
+++ NASA TND889.py
@@ -1,15 +1,8 @@
 '''
-Compared with page 4 of https://arc.aiaa.org/doi/pdf/10.2514/3.62081 - NASA Flight TND 889
+Compared with page 4 of https://arc.aiaa.org/doi/pdf/10.2514/3.62081 - NASA TND 889
 
 All of these values are at a point between station 9 and 10
 
-Example of the values at a point:
------------------------------------
-At the peak heat transfer rate, reading off the NASA graph we have approximately:
-alt = 6096 m
-Vinf = 1280.16 m/s - this gives a Mach number of about 4.05 I think
-q_turb = 44 Btu/ft^2/s = 499.69 kW/m^2
-q_lam = 4 Btu/ft^2/s = 45.43 kW/m^2
 '''
 
 import trajectory, trajectory.post, trajectory.aero, csv
@@ -25,6 +18,7 @@ Set up the flight conditions
 - I used http://www.graphreader.com/ to convert the graphs in the NASA document into actual data that we can use.
 - The data in the dictionaries below is all in metric SI.
 '''
+
 rex_data = {"x":[15.5,16,16.5,17,17.5,18,18.5,19,19.5,20,20.5,21,21.5,22,22.5,23,23.5,24,24.5,25,25.5,26,26.5,27,27.5,28,28.5,29,29.5,30,30.5,31,31.5,32,32.5,33,33.5,34,34.5,35,35.5,36,36.5],"y":[12102505.652,13160445.416,14353494.495,15654443.989,18090353.726,18387087.432,17396000.512,16458334.411,15571209.682,14786837.292,14205883.059,13647531.886,13111126.27,12595803.66,12100735.404,11877040.443,11848696.852,11820613.027,11792404.096,11764453.696,11736378.784,11708370.872,11465622.72,11156778.823,10856254.105,10541869.776,10136587.292,9418130.544,8473605.389,7623804.741,5304445.602,3588480.481,3374700.953,3186164.8,3023258.58,2868681.634,2319618.002,1899266.388,1605792.45,1357665.997,1239427.639,1135705.901,1029794.548]}
 qlam_data = {"x":[15,15.5,16,16.5,17,17.5,18,18.5,19,19.5,20,20.5,21,21.5,22,22.5,23,23.5,24,24.5,25,25.5,26,26.5,27,27.5,28,28.5,29,29.5,30,30.5,31,31.5,32,32.5,33,33.5,34,34.5,35,35.5,36,36.5],"y":[3582.504,2489.143,7963.705,12878.017,19021.876,37116.14,46200.347,39452.197,32962.191,27801.511,25254.8,22708.089,20161.378,17614.666,14800.447,12538.765,12538.765,14428.885,16481.846,18946.148,22356.151,25766.155,29176.158,31650.882,33566.979,35483.076,37399.173,39315.27,41687.322,42990.051,40514.866,36682.672,34895.491,35893.937,44663.696,57153.97,72099.527,69749.337,62811.744,59048.285,55328.966,51409.676,45907.031,41198.799]}
 qturb_data = {"x":[15,15.5,16,16.5,17,17.5,18,18.5,19,19.5,20,20.5,21,21.5,22,22.5,23,23.5,24,24.5,25,25.5,26,26.5,27,27.5,28,28.5,29,29.5,30,30.5,31,31.5,32,32.5,33,33.5,34,34.5,35,35.5,36,36.5],"y":[34563.378,49540.033,107993.496,168992.164,223838.211,433950.451,483862.019,425903.936,380037.93,334171.924,288305.918,242439.912,204684.323,179168.54,153652.757,129195.956,138541.853,147887.751,164395.429,183532.266,202669.103,221110.689,238333.842,255556.996,273677.602,291838.07,300624.77,308468.141,306235.898,282063.051,257870.056,233365.569,208861.083,226843.02,259098.536,299653.194,347476.595,301548.186,255619.778,223764.208,193810.898,166804.391,143840.187,123294.458]}
@@ -62,15 +56,15 @@ ax4.legend(loc = 'upper right')
 plt.show()
 
 '''Specify the nosecone and create the HeatTransfer analysis object'''
-#The nosecone is the same as that used in Reference 8 (pg A-3) - xprime = 2.504 ft = 0.7632192 m , yprime = 0.25 ft = 0.0762 m
+#Nosecone dimensions from the NASA documents: xprime = 2.504 ft = 0.7632192 m , yprime = 0.25 ft = 0.0762 m
 tangent_ogive = trajectory.post.TangentOgive(xprime = 0.7632192, yprime = 0.0762)
 analysis = trajectory.post.HeatTransfer(tangent_ogive, trajectory_data, martlet4)
 
 '''Run the simulation'''
 #analysis.step()
 #analysis.run()
-#analysis.to_json("NQLDW019 Reference 8.json")
-analysis.from_json("NQLDW019 Reference 8.json")
+#analysis.to_json("NASA TND889.json")
+analysis.from_json("NASA TND889.json")
 
 '''Plot graphs'''
 #analysis.plot_fluid_properties(automatic_rescaling=True)
@@ -79,16 +73,16 @@ analysis.from_json("NQLDW019 Reference 8.json")
 
 #Plot the heat transfer rates we generated against the data provided by NASA
 fig, ax1 = plt.subplots()
-fig.suptitle("NASA Flight TND 889")
+fig.suptitle("NASA TND 889")
 
 #Plot heat transfer rates
-ax1.plot(time_data, analysis.q_lam[9, :], label="QLAM (Python)", color="blue", linestyle="--")
-ax1.plot(qlam_data["x"], qlam_data["y"], label="QLAM (NASA)", color="blue")
-ax1.plot(time_data, analysis.q_turb[9, :], label="QTURB (Python)", color="red", linestyle='--')
-ax1.plot(qturb_data["x"], qturb_data["y"], label="QTURB (NASA)", color="red")
+ax1.plot(time_data, analysis.q_lam[9, :]/1000, label="QLAM (Python)", color="blue", linestyle="--")
+ax1.plot(qlam_data["x"], np.array(qlam_data["y"])/1000, label="QLAM (NASA)", color="blue")
+ax1.plot(time_data, analysis.q_turb[9, :]/1000, label="QTURB (Python)", color="red", linestyle='--')
+ax1.plot(qturb_data["x"], np.array(qturb_data["y"])/1000, label="QTURB (NASA)", color="red")
 
 ax1.set_xlabel("Time (s)")
-ax1.set_ylabel("Heat transfer rate (W/m^2)")
+ax1.set_ylabel("Heat transfer rate (kW/m^2)")
 ax1.grid()
 ax1.legend()
 
