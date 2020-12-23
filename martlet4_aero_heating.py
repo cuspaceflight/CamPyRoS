c20fcc2e12294500cb567cdcd2b00cd101e345b0
dug20 <daniel.u.gibbons@gmail.com>
2020-12-23 00:09:19 +0000

    Cleaned up plots and examples

--- martlet4_aero_heating.py
+++ martlet4_aero_heating.py
@@ -8,11 +8,11 @@ tangent_ogive = trajectory.post.TangentOgive(xprime = 73.7e-2, yprime = (19.7e-2
 analysis = trajectory.post.HeatTransfer(tangent_ogive, trajectory_data, martlet4)
 analysis.step(print_style="metric")
 
-#analysis.run(iterations = 300, print_style="minimal")
+#analysis.run(iterations = 150, starting_index = 120, print_style="minimal")
 #analysis.to_json("martlet4_heating.json")
 
 analysis.from_json("martlet4_heating.json")
 
-analysis.plot_station(station_number = 10, imax = 300)
-analysis.plot_heat_transfer(automatic_rescaling=True)
-analysis.plot_fluid_properties(automatic_rescaling=True)
+analysis.plot_station(station_number = 9, imax = 300)
+#analysis.plot_heat_transfer(automatic_rescaling=True)
+#analysis.plot_fluid_properties(automatic_rescaling=True)
