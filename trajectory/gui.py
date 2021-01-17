793896cb3eed355958caedd59f3c03f22160c33c
dug20 <daniel.u.gibbons@gmail.com>
2021-01-17 15:16:07 +0000

    Can now generate simulation code with GUI
    Can't do everything, but the general skeleton is in place

--- trajectory/gui.py
+++ trajectory/gui.py
@@ -1,4 +1,6 @@
-import json, subprocess
+import json
+import ast
+import numpy as np
 
 import tkinter as tk
 import tkinter.filedialog
@@ -16,22 +18,23 @@ OBJECTS_DICTIONARY = {"Launch Site" : LaunchSite,
                       "Wind" : Wind, 
                       "Rocket" : Rocket}
 
-#Possible inputs and the datatype, for each object
-LAUNCH_SITE_INPUTS = {"Rail length (m)" : float,
-                        "Rail Yaw (deg)" : float, 
-                        "Rail Pitch (deg)" : float, 
-                        "Altitude (m)" : float, 
-                        "Longitude (deg)" : float, 
-                        "Latitude (deg)" : float, 
-                        "Variable Wind?" : bool,
-                        "Forecast plus time" : float,
-                        "Run Date (yyyymmdd)" : int,
-                        "Fast wind?" : bool}
-PARACHUTE_INPUTS = {"Not yet implemented" : str}
-MOTOR_INPUTS = {"Not yet implemented" : str}
-RASAERODATA_INPUTS = {"Not yet implemented" : str}
-WIND_INPUTS = {"Not yet implemented" : str}
-ROCKET_INPUTS = {"Not yet implemented" : str}
+#Possible inputs and the datatype, for each object. Values are in the format [datatype, default value]
+LAUNCH_SITE_INPUTS = {"Rail length (m)" : [float, 5],
+                        "Rail Yaw (deg)" : [float, 0], 
+                        "Rail Pitch (deg)" : [float, 0], 
+                        "Altitude (m)" : [float, 0], 
+                        "Longitude (deg)" : [float, 0], 
+                        "Latitude (deg)" : [float, 0], 
+                        "Variable Wind?" : [bool],
+                        "Forecast plus time" : [str, "016"],
+                        "Run Date (yyyymmdd)" : [str, "20200101"],
+                        "Fast wind?" : [bool],
+                        "Default wind [south, east, up] (m/s)" : [list, [0, 0, 0]]}
+PARACHUTE_INPUTS = {"Not yet implemented" : [str, ""]}
+MOTOR_INPUTS = {"Not yet implemented" : [str, ""]}
+RASAERODATA_INPUTS = {"Not yet implemented" : [str, ""]}
+WIND_INPUTS = {"Not yet implemented" : [str, ""]}
+ROCKET_INPUTS = {"Not yet implemented" : [str, ""]}
 
 INPUTS_DICTIONARY = {"Launch Site" : LAUNCH_SITE_INPUTS,
                      "Parachute" : PARACHUTE_INPUTS, 
@@ -43,6 +46,9 @@ INPUTS_DICTIONARY = {"Launch Site" : LAUNCH_SITE_INPUTS,
 #Keep track of the objects and methods that the user has made:
 current_objects = {}
 current_methods = {}
+
+#Keep track of the code
+current_code = "import trajectory\n\n"
 
 '''CLASSES'''
 #Main window and functions
@@ -91,10 +97,10 @@ class Main:
         self.steps_list.pack(expand=True, fill='both')
         
         #Code tab text
-        code_text = tk.Text(code_tab)
-        code_text.insert('1.0', "The generated code is shown here")
-        code_text.config(state='disabled')
-        code_text.pack(expand=True, fill='both')
+        self.code_text = tk.Text(code_tab)
+        self.code_text.insert('1.0', "The generated code is shown here")
+        self.code_text.config(state='disabled')
+        self.code_text.pack(expand=True, fill='both')
         
     def open(self):
         popup_message("'Open' is not yet implemented")
@@ -114,7 +120,17 @@ class Main:
         save_file.close()
 
     def generate_code(self):
-        popup_message("'Generate code' is not yet functional")
+        global current_code
+        current_code = "import trajectory\n\n"
+
+        object_names = list(current_objects.keys())
+        for i in range(len(object_names)):
+            object_inputs = current_objects[object_names[i]]
+            code_to_add = object_to_code(object_names[i].replace(" ", "_"), object_inputs)
+            current_code = current_code + code_to_add + "\n"
+        
+        refresh_code_display()
+        popup_message("Finished generating code")
 
 #List of objects
 class ObjectsList(tk.Listbox):
@@ -169,7 +185,7 @@ class AddObjectWindow(tk.Toplevel):
         self.title("Add object")
 
         #Set up the Listbox
-        self.options_list = tk.Listbox(self)
+        self.options_list = tk.Listbox(self, exportselection=False)
         for i in range(len(OBJECTS_DICTIONARY.keys())):
             self.options_list.insert(i+1, list(OBJECTS_DICTIONARY.keys())[i])
         self.options_list.pack(side="left", expand=True, fill="both")
@@ -193,8 +209,6 @@ class AddObjectWindow(tk.Toplevel):
         self.name_entry.grid(column = 1, row = 0)
         self.add_button.grid(column = 2, row = 0)
 
-        #Entry box 
-
     def show_inputs(self, event):
         self.object_key = self.options_list.get(self.options_list.curselection())
         self.inputs = INPUTS_DICTIONARY[self.object_key]
@@ -211,14 +225,21 @@ class AddObjectWindow(tk.Toplevel):
             self.input_labels.append(tk.Label(self.inputs_frame, text=self.input_keys[i]))
             self.input_labels[i].grid(column = 0, row = i)
 
-            datatype = self.inputs[self.input_keys[i]]
-            if datatype == float or datatype == int or datatype == str:
+            datatype = self.inputs[self.input_keys[i]][0]
+            if datatype == float or datatype == int or datatype == str or datatype == list:
                 self.input_entries.append(tk.Entry(self.inputs_frame))
                 self.input_entries[i].grid(column = 1, row = i)
+
+                default_value = self.inputs[self.input_keys[i]][1]
+                self.input_entries[i].insert(0, str(default_value))
 
             elif datatype == bool:
                 self.input_entries.append(TrueFalse(self.inputs_frame))
                 self.input_entries[i].grid(column = 1, row = i)
+            else:
+                self.input_entries.append(tk.Entry(self.inputs_frame))
+                self.input_entries[i].grid(column = 1, row = i)  
+
 
     def add_object(self):
         #popup_message("The 'Add object' button is not yet functional")
@@ -234,12 +255,28 @@ class AddObjectWindow(tk.Toplevel):
 
         #Save what the user entered for each input
         for i in range(len(self.input_entries)):
-            datatype = self.inputs[self.input_keys[i]]
+            datatype = self.inputs[self.input_keys[i]][0]
 
             if datatype == float or datatype == int or datatype == str:
                 dictionary_to_save[self.input_keys[i]] = datatype(self.input_entries[i].get())
+
             elif datatype == bool:
                 dictionary_to_save[self.input_keys[i]] = bool(self.input_entries[i].var.get())
+
+            elif datatype == list:
+                try:
+                    literal_eval = ast.literal_eval(self.input_entries[i].get())
+                except SyntaxError:
+                    popup_message(f"Failed to convert {self.input_entries[i].get()} to a list")
+                    raise
+
+                if type(literal_eval) is list:
+                    dictionary_to_save[self.input_keys[i]] = literal_eval
+                else:
+                    popup_message(f"Failed to convert {self.input_entries[i].get()} to a list")
+                    raise TypeError(f"Failed to convert {self.input_entries[i].get()} to a list")
+            else:
+                dictionary_to_save[self.input_keys[i]] = str(self.input_entries[i].get())
 
         #Save everything to the 'current_objects' dictionary
         current_objects[object_name] = dictionary_to_save
@@ -262,7 +299,6 @@ class TrueFalse(tk.Frame):
         self.RFalse = tk.Radiobutton(self, text="False", variable=self.var, value=0)
         self.RFalse.grid(column = 1, row = 0)
 
-
 '''FUNCTIONS'''
 #Call this function to make a popup message
 def popup_message(message):
@@ -284,6 +320,31 @@ def refresh_objects_list():
     #Repopulate the list
     for i in range(len(objects_list)):
         main_gui.objects_list.insert(i+1, objects_list[i])
+
+def refresh_code_display():
+    main_gui.code_text.config(state='normal')
+    main_gui.code_text.delete('1.0', "end")
+    main_gui.code_text.insert('1.0', current_code)
+    main_gui.code_text.config(state='disabled')
+
+#Takes the users inputs and returns the Python code needed to generate the object
+def object_to_code(name, dictionary):
+    if dictionary["__OBJECT__"] == "Launch Site":
+        return (f'{name} = trajectory.LaunchSite('
+                f'rail_length={dictionary["Rail length (m)"]}, '
+                f'rail_yaw={dictionary["Rail Yaw (deg)"]}, '
+                f'rail_pitch={dictionary["Rail Pitch (deg)"]}, '
+                f'alt={dictionary["Altitude (m)"]}, '
+                f'longi={dictionary["Longitude (deg)"]}, '
+                f'lat={dictionary["Latitude (deg)"]}, '
+                f'variable_wind={dictionary["Variable Wind?"]}, '
+                f'forcast_plus_time="{dictionary["Forecast plus time"]}", '
+                f'run_date="{dictionary["Run Date (yyyymmdd)"]}", '
+                f'fast_wind={dictionary["Fast wind?"]} '
+                f'default_wind={dictionary["Default wind [south, east, up] (m/s)"]})')
+    else:
+        popup_message("Converting the object type '{}' to code has not yet been implemented. This object has been skipped".format(dictionary["__OBJECT__"]))
+
 
 root = tk.Tk()
 root.state('zoomed')
