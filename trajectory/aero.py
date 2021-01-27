8c9990979f4852c1004b038f7a8e04bd1d4f40ef
dug20 <daniel.u.gibbons@gmail.com>
2021-01-27 15:49:31 +0000

    Working on AeroData interpolation
    Still need to check if the data is being read and interpolated (in 2D) properly, and if the aero functions are working right. Was having trouble getting the input data arrays into the right shapes for np.interpolation.interp2d

--- trajectory/aero.py
+++ trajectory/aero.py
@@ -2,48 +2,87 @@ import scipy.interpolate, csv
 import numpy as np
 
 class AeroData:
-    """Object holding aerodynamic data for the rocket
+    """Object holding aerodynamic data for the rocket.
 
-    Note
-    ----
-    Relies on an axially symetric body
+    Assumes an axially symmetric body. Uses scipy.interpolate.interp2d to interpolate data from arrays.
 
-    Attributes
-    ----------
-    ref_area : float, optional
-        Referance area used to normalise coefficients, /m^2
-    pitch_damping_coefficient : float
-        Pitch damping coefficient, defined by moment = C * ρ * ω^2.  Defaults to zero.
-    roll_damping_coefficient : float
-        Roll damping coefficient, defined by moment = C * ρ * ω^2. Defaults to zero.
-    COP : function(Mach, alpha)
-        Distance between the nose tip and the centre of pressure (m), as a function of Mach number and angle of attack.
-    CA : function(Mach, alpha)
-       Axial coefficient of drag, as a function of Mach number and angle of attack.
-    CN : function(Mach, alpha)
-        Normal coefficient of drag, as a function of Mach number and angle of attack. 
-    
-    """ 
-    def __init__(self, CA, CN, COP, ref_area, pitch_damping_coefficient = 0, roll_damping_coefficient = 0):
+    Args:
+        CA_grid (array, 2D): Axial force coefficient data.
+        CN_grid (array, 2D): Normal force coefficient data.
+        COP_grid (array, 2D): Centre of pressure data, containing distances between the nose tip and the centre of pressure (m).
+        Mach_list (array, 1D): Mach number data.
+        alpha_list (array, 1D): Angle of attack data (radians).
+        ref_area (float): Referance area used to normalise coefficients (m^2).
+        pitch_damping_coefficient (float, optional): Pitch damping coefficient, defined by moment = C * ρ * ω^2.  Defaults to zero.
+        roll_damping_coefficient (float, optional): Roll damping coefficient, defined by moment = C * ρ * ω^2. Defaults to zero.
+        error(dictionary, optional): Used for running stochastic analyses.
+
+    Attributes:
+        CA_grid (array): Axial force coefficient data.
+        CN_grid (array): Normal force coefficient data.
+        COP_grid (array): Centre of pressure data, containing distances between the nose tip and the centre of pressure (m).
+        Mach_list (array): Mach number data.
+        alpha_list (array): Angle of attack data (radians).
+        ref_area (float): Reference area used to normalise coefficients (m^2).
+        pitch_damping_coefficient (float): Pitch damping coefficient, defined by moment = C * ρ * ω^2. 
+        roll_damping_coefficient (float): Roll damping coefficient, defined by moment = C * ρ * ω^2. 
+        error(dictionary): Used for running stochastic analyses.
+    """
+
+    def __init__(self, CA_grid, CN_grid, COP_grid, Mach_list, alpha_list, ref_area, pitch_damping_coefficient = 0.0, roll_damping_coefficient = 0.0, error={"CA":1.0,"CN":1.0,"COP":1.0}):
         self.ref_area = ref_area
         self.pitch_damping_coefficient = pitch_damping_coefficient
         self.roll_damping_coefficient = roll_damping_coefficient
 
-        self.CA = CA
-        self.CN = CN
-        self.COP = COP
+        self.Mach_list = Mach_list
+        self.alpha_list = alpha_list
+        self.CA_grid = CA_grid
+        self.CN_grid = CN_grid
+        self.COP_grid = COP_grid
+
+        self.error = error
+
+        #These can be overridden to custom functions if you wanted - in which case the _grid attributes are irrelevant.
+        self.CA_func = scipy.interpolate.interp2d(self.Mach_list, self.alpha_list, self.CA_grid)
+        self.CN_func = scipy.interpolate.interp2d(self.Mach_list, self.alpha_list, self.CN_grid)
+        self.COP_func = scipy.interpolate.interp2d(self.Mach_list, self.alpha_list, self.COP_grid)
+
+    def CA(self, Mach, alpha):
+        return self.CA_func(Mach, alpha)
+
+    def CN(self, Mach, alpha):
+        return self.CN_func(Mach, alpha)
+
+    def COP(self, Mach, alpha):
+        return self.COP_func(Mach, alpha)
 
     @staticmethod
-    def from_arrays(CA_data, CN_data, COP_data, Mach_data, alpha_data, ref_area, pitch_damping_coefficient = 0, roll_damping_coefficient = 0):
-        #Generate the functions from the arrays, using scipy.interpolate.interp2d
-        COP_function = scipy.interpolate.interp2d(Mach_data, alpha_data, COP_data)
-        CA_function = scipy.interpolate.interp2d(Mach_data, alpha_data, CA_data)
-        CN_function = scipy.interpolate.interp2d(Mach_data, alpha_data, CN_data)
+    def from_lists(CA_list, CN_list, COP_list, Mach_list, alpha_list, ref_area, pitch_damping_coefficient = 0, roll_damping_coefficient = 0, error={"CA":1.0,"CN":1.0,"COP":1.0}):
+        #Convert into the right shapes
+        Mach_list = np.unique(Mach_list)
+        alpha_list = np.unique(alpha_list)
 
-        return AeroData(CA_function, CN_function, COP_function, ref_area, pitch_damping_coefficient, roll_damping_coefficient)
+        CA_grid = np.reshape(CA_list, (len(alpha_list), len(Mach_list)))
+        CN_grid = np.reshape(CN_list, (len(alpha_list), len(Mach_list)))
+        COP_grid = np.reshape(COP_list, (len(alpha_list), len(Mach_list)))
+
+        return AeroData(CA_grid, CN_grid, COP_grid, Mach_list, alpha_list, ref_area, pitch_damping_coefficient, roll_damping_coefficient, error)
+
 
     @staticmethod
     def from_rasaero(csv_directory, ref_area, pitch_damping_coefficient = 0, roll_damping_coefficient = 0, error={"CA":1.0,"CN":1.0,"COP":1.0}): 
+        """Convert an aerodynamic data .CSV file from RASAero II into an AeroData object.
+
+        Args:
+            csv_directory (string): Directory to .CSV file.
+            ref_area (float): Referance area used to normalise coefficients (m^2).
+            pitch_damping_coefficient (float, optional): Pitch damping coefficient, defined by moment = C * ρ * ω^2.  Defaults to zero.
+            roll_damping_coefficient (float, optional): Roll damping coefficient, defined by moment = C * ρ * ω^2. Defaults to zero.
+            error(dictionary, optional): Used for running stochastic analyses.
+
+        Returns:
+            AeroData: AeroData object.
+        """
         with open(csv_directory) as csvfile:
             aero_data = csv.reader(csvfile)
         
@@ -61,54 +100,26 @@ class AeroData:
                 CA_raw.append(float(row[5]))
                 COP_raw.append(float(row[12]))
                 CN_raw.append(float(row[8]))
-        
-        #Seperate the data by angle of attack.
-        Mach = []
-        CA_0 = []  #CA at alpha = 0
-        CA_2 = []  #CA at alpha = 2
-        CA_4 = []  #CA at alpha = 4
-        COP_0 = []
-        COP_2 = []
-        COP_4 = []
-        CN_0 = []
-        CN_2 = []
-        CN_4 = []
-             
-        for i in range(len(Mach_raw)):
-            if alpha_raw[i] == 0:
-                Mach.append(Mach_raw[i])
-                CA_0.append(CA_raw[i])
-                COP_0.append(COP_raw[i])
-                CN_0.append(CN_raw[i])
-             
-            elif alpha_raw[i] == 2:
-                CA_2.append(CA_raw[i])
-                COP_2.append(COP_raw[i])
-                CN_2.append(CN_raw[i])    
-              
-            elif alpha_raw[i] == 4:
-                CA_4.append(CA_raw[i])
-                COP_4.append(COP_raw[i])
-                CN_4.append(CN_raw[i])   
-           
-        #Make sure all the lists are the same length - this is needed because it seems the alpha=4 data only has 2499 points, but the others have 2500
-        CA_0, CA_2, CA_4 = CA_0[:2498], CA_2[:2498], CA_4[:2498]
-        CN_0, CN_2, CN_4 = CN_0[:2498], CN_2[:2498], CN_4[:2498]
-        COP_0, COP_2, COP_4 = COP_0[:2498], COP_2[:2498], COP_4[:2498]
-        Mach = Mach[:2498]
-           
-        #Generate grids of the data
-        CA = error["CA"]*np.array([CA_0, CA_2, CA_4])
-        CN = error["CN"]*np.array([CN_0, CN_2, CN_4])
-        COP = error["COP"]*0.0254*np.array([COP_0, COP_2, COP_4])    #Convert inches to m
-        alpha = [0,2,4]
-                    
-        #Generate functions (note these are funcitons, not variables) which return a coefficient given (Mach, alpha)
-        COP_function = scipy.interpolate.interp2d(Mach, alpha, COP)
-        CA_function = scipy.interpolate.interp2d(Mach, alpha, CA)
-        CN_function = scipy.interpolate.interp2d(Mach, alpha, CN)
 
-        return AeroData(CA_function, CN_function, COP_function, ref_area, pitch_damping_coefficient, roll_damping_coefficient)
+        #The data has length 7499 when it should be 7500 (3x2500).  We'll just add the last datapoint on twice.
+        Mach_raw.append(Mach_raw[-1])
+        alpha_raw.append(alpha_raw[-1])
+        CA_raw.append(CA_raw[-1])
+        COP_raw.append(COP_raw[-1])
+        CN_raw.append(CN_raw[-1])
+
+        #Convert alpha from degrees to radians
+        alpha_raw = np.array(alpha_raw) * np.pi/180
+
+        #Convert into the right forms
+        Mach_list = np.unique(Mach_raw)
+        alpha_list = np.unique(alpha_raw)
+
+        CA_grid = np.reshape(CA_raw, (len(alpha_list), len(Mach_list)))
+        CN_grid = np.reshape(CN_raw, (len(alpha_list), len(Mach_list)))
+        COP_grid = np.reshape(COP_raw, (len(alpha_list), len(Mach_list)))
+
+        return AeroData(CA_grid, CN_grid, COP_grid, Mach_list, alpha_list, ref_area, pitch_damping_coefficient, roll_damping_coefficient, error)
 
 def pitch_damping_coefficient(length, radius, fin_number, area_per_fin):
     """Gives approximate values for the pitch damping coefficient. Uses equations (3.59) and (3.60) from the OpenRocket documentation.
