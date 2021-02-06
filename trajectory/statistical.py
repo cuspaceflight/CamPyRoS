209d9864ee187ce90c6eee9c416546032b310c06
Jago Strong-Wright <jagoosw@protonmail.com>
2021-02-06 23:12:22 +0000

    Changed some licencing and removed old novus

--- trajectory/statistical.py
+++ trajectory/statistical.py
@@ -13,7 +13,7 @@ from datetime import date
 
 __copyright__ = """
 
-    Copyright 2021 Jago Strong-Wright & Daniel Gibbons
+    Copyright 2021 Jago Strong-Wright
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
