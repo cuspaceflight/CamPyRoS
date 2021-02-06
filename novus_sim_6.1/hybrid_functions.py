66912965929be519ebeeb94da03bb20f7800ef96
Jago Strong-Wright <jagoosw@protonmail.com>
2021-02-06 23:12:22 +0000

    Changed some licencing and removed old novus

--- novus_sim_6.1/hybrid_functions.py
+++ novus_sim_6.1/hybrid_functions.py
@@ -3,6 +3,27 @@
 ### Joe Hunt updated 20/06/19 ###
 ### All units SI unless otherwise stated ###
 
+__copyright__ = """
+
+    Copyright 2019 Joe Hunt
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Modified by Daniel Gibbons 2021 (changes documented in commit history)
+
+"""
+
 import numpy as np
 import scipy.optimize
 
--- novus_sim_6.1/trajectory_sim.py
+++ novus_sim_6.1/trajectory_sim.py
@@ -1,5 +1,24 @@
 """Simple 3DOF Martlet 4 trajectory simulator"""
 
+__copyright__ = """
+
+    Copyright 2019 Joe Hunt
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+
 ### Joe Hunt updated 20/06/19 ###
 ### All units SI unless otherwise stated ###
 
