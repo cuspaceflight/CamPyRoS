6a9cf65bffdca7f8e241b24895ae2c5ea2471fe1
dug20 <daniel.u.gibbons@gmail.com>
2021-02-08 13:56:23 +0000

    Deleted old files in legacy

--- legacy/output_test.py
+++
@@ -1,4 +0,0 @@
-from main import plot_altitude_time
-import pandas as pd
-record = pd.DataFrame({"Time":[0,1,2,3,4,5],"x":[0,1,2,3,4,5],"y":[0,1,2,3,4,5],"z":[0,1,2,3,4,5],"v_x":[0,1,2,3,4,5],"v_y":[0,1,2,3,4,5],"v_z":[0,1,2,3,4,5]})
-plot_altitude_time(record)
